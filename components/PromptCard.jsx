68999106c369b092541639acc31dddb37022659d
James Hudson <jmh357@comcast.net>
2023-09-20 16:42:10 -0500

    add update functionality and routes for update / delete

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,9 +6,11 @@ import { useSession } from 'next-auth/react'
 import { usePathname, useRouter } from 'next/navigation'
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession()
+  const pathName = usePathname()
+  const router = useRouter()
+  
   const [copied, setCopied] = useState('')
-  console.log('copied', copied)
-  console.log('hi', post.prompt)
 
   const handleCopy = () => {
     setCopied(post.prompt)
@@ -58,8 +60,25 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         className='foont-inter text-sm blue_gradient cursor-pointer'
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p
+            className='font-inter text-sm green_gradient cursor-pointer'
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className='font-inter text-sm orange_gradient cursor-pointer'
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
